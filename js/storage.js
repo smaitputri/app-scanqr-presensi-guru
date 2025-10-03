23040bd64b8f3050e4ee8d963ce7bd03eb499a2d
smaitputri <smaislamterpadumataram@gmail.com>
2025-10-03 09:27:32 +0800

    update storage function 2nd

--- js/storage.js
+++ js/storage.js
@@ -1,41 +1,22 @@
 // Simpan presensi ke localStorage
-// function savePresencesToLocalStorage() {
-//     if (currentTeacher) {
-//         localStorage.setItem(`teacherPresences_${currentTeacher.id}`, JSON.stringify(teacherPresences));
-//     }
-//     localStorage.setItem('allPresences', JSON.stringify(allPresences));
-// }
+function savePresencesToLocalStorage() {
+    if (currentTeacher) {
+        localStorage.setItem(`teacherPresences_${currentTeacher.id}`, JSON.stringify(teacherPresences));
+    }
+    localStorage.setItem('allPresences', JSON.stringify(allPresences));
+}
 
 // Load presensi dari localStorage
-// function loadPresencesFromLocalStorage() {
-//     if (currentTeacher) {
-//         const savedPresences = localStorage.getItem(`teacherPresences_${currentTeacher.id}`);
-//         if (savedPresences) {
-//             teacherPresences = JSON.parse(savedPresences);
-//         }
-//     }
-    
-//     const savedAllPresences = localStorage.getItem('allPresences');
-//     if (savedAllPresences) {
-//         allPresences = JSON.parse(savedAllPresences);
-//     }
-// }
-
-function savePresencesToLocalStorage() {
-    const today = new Date().toDateString();
-    
-    // Filter hanya data hari ini dan batasi 10
+function loadPresencesFromLocalStorage() {
     if (currentTeacher) {
-        const todayPresences = teacherPresences
-            .filter(presence => new Date(presence.timestamp).toDateString() === today)
-            .slice(-10);
-        
-        localStorage.setItem(`teacherPresences_${currentTeacher.id}`, JSON.stringify(todayPresences));
+        const savedPresences = localStorage.getItem(`teacherPresences_${currentTeacher.id}`);
+        if (savedPresences) {
+            teacherPresences = JSON.parse(savedPresences);
+        }
     }
     
-    const todayAllPresences = allPresences
-        .filter(presence => new Date(presence.timestamp).toDateString() === today)
-        .slice(-10);
-    
-    localStorage.setItem('allPresences', JSON.stringify(todayAllPresences));
+    const savedAllPresences = localStorage.getItem('allPresences');
+    if (savedAllPresences) {
+        allPresences = JSON.parse(savedAllPresences);
+    }
 }
